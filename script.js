ce5a85a8c8b76015f42456d0cb61fb46b7ec14cd
duybui1311 <117589301+duybui1311@users.noreply.github.com>
2023-08-01 22:57:47 +0700

    added back to top button

--- script.js
+++ script.js
@@ -6,3 +6,20 @@ hamburger.addEventListener("click", () => {
 });
 
 
+function scrollToTop() {
+  document.body.scrollTop = 0; // For Safari
+  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE, and Opera
+}
+
+window.onscroll = function() {
+  showBackToTopButton();
+};
+
+function showBackToTopButton() {
+  var button = document.getElementById('backToTopBtn');
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    button.classList.add('show');
+  } else {
+    button.classList.remove('show');
+  }
+}

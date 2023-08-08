0df99883015a9abb822359bc9f0a83e4d29b02b8
duybui1311 <117589301+duybui1311@users.noreply.github.com>
2023-08-08 20:40:25 +0700

    formatted and fixed small issues

--- script.js
+++ script.js
@@ -15,8 +15,8 @@ hamburger.addEventListener("click", () => {
 
 
 function scrollToTop() {
-  document.body.scrollTop = 0; // For Safari
-  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE, and Opera
+  document.body.scrollTop = 0; 
+  document.documentElement.scrollTop = 0; 
 }
 
 window.onscroll = function() {
